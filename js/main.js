9494207b157ba2c9e8cf91ba6c177e3988d7b2b9
richernote <richernote@gmail.com>
2022-07-02 02:13:18 -0700

    Changed style of page from auto loading the next drink to showing all of them. created function to parse through array of drinks and make elements.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
 document.querySelector("form").addEventListener('submit', startDrink)
-
+const back = document.querySelector("#back")
+const forward = document.querySelector("#forward")
 
 
 function startDrink(event) {
@@ -16,53 +17,54 @@ function startDrink(event) {
         alert('Nothing entered')
         return
     }
-    console.log('Starting')
+
     getDrink(drinkName)
 
 }
 
 function getDrink(drinkName) {
-    console.log(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName.toLowerCase()}`)
+
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName.toLowerCase()}`)
         .then(rez => rez.json())
         .then(data => {
-            setDrink(data.drinks[0])
-            let rotation = swapDrinks(data.drinks);
-            document.querySelector("button").addEventListener('click', () =>{
-                if(!rotation) {
-                    rotation;
-                } else {
-                    clearInterval(rotation);
-                    rotation;
-                }
-            })
+
+            const drinks = data.drinks
+            const box = document.getElementById('box')
+            
+            if(box.childElementCount > 0) {
+                box.replaceChildren();
+                drinkPopulate(drinks);
+            } else {
+                drinkPopulate(drinks)
+            }
+
         })
         .catch(err => alert("Are you sure thats a drink?"))
-
 }
 
-function swapDrinks(drinks) {
 
-    let i = 1;
+function drinkPopulate(drinks) {
 
-    return interval = setInterval(() => {
-        
-        
-        if(drinks.length === 1) {
-            return;
-        }
-        
-        i < drinks.length -1 ? i++ : i = 0;
-        
-        let drink = drinks[i]
-        
-        setDrink(drink);
-    }, 4000)
+    return drinks.map((drink) => {
+
+        let drinkElement = document.createElement('div');
+        drinkElement.classList.add('card')
+        let drinkTitle = document.createElement('h2');
+        let drinkImage = document.createElement('img');
+        let drinkInstructions = document.createElement('p');
+
+        setDrink(drink, drinkTitle, drinkImage, drinkInstructions)
+
+        drinkElement.appendChild(drinkTitle)
+        drinkElement.appendChild(drinkImage)
+        drinkElement.appendChild(drinkInstructions)
+
+        document.getElementById('box').appendChild(drinkElement)
+    })
 }
 
-function setDrink(drink) {
-    console.log(drink)
-    document.querySelector('img').src = drink.strDrinkThumb;
-    document.querySelector("h2").innerText = drink.strDrink;
-    document.querySelector('h3').innerText = drink.strInstructions
+function setDrink(drink, name, img, instructions) {
+    img.src = drink.strDrinkThumb;
+    name.innerText = drink.strDrink;
+    instructions.innerText = drink.strInstructions
 }
